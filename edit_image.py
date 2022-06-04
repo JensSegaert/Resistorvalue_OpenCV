1c2eb1e7e5710de8df5c6844bf63b41a80458771
JensSegaert <100967939+JensSegaert@users.noreply.github.com>
2022-06-04 11:22:54 +0200

    Update edit_image.py

--- edit_image.py
+++ edit_image.py
@@ -11,7 +11,11 @@ from PIL import Image, ImageEnhance
 
 # Function to give color balance to input image and show result image
 def edit_input_image(path_image):
+    
+    # Open image
     img = Image.open(path_image)
+    
+    # Apply color balance on image
     enhancer = ImageEnhance.Color(img)
 
     # Color balance the image with factor 2.5
@@ -19,11 +23,16 @@ def edit_input_image(path_image):
 
     colored_output = enhancer.enhance(factor)
 
-    # Show both input image and result image
+    # Show input image
     img.show()
+    
+    # Show output image
     colored_output.show()
+    
     return
 
-# Call function (input: given path of image that needs to be editted)
+
+
+# Call function (input: given path of image that needs to be editted with color balance)
 edit_input_image(os.path.abspath(os.getcwd()) + '\input_images\img_4.png')
 
